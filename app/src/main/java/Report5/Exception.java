9a50e1b80954a96919a6c6a797317d518ff501f0
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2021-12-14 23:01:25 +0900

    1st commit

---
+++ app/src/main/java/Report5/Exception.java
@@ -0,0 +1,10 @@
+package Report5;
+
+public class Exception {
+     public static void main(String[] args){
+        String str;
+        str = null;
+        System.out.println(str.length());
+     }
+
+}
