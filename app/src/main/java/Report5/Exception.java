473eb65fe48be8ede3228980d8aba97a6ac92c6b
Taiga Nishino <e215736@ie.u-ryukyu.ac.jp>
2021-12-15 22:07:23 +0900

    2nd commit

--- app/src/main/java/Report5/Exception.java
+++ app/src/main/java/Report5/Exception.java
@@ -4,7 +4,12 @@ public class Exception {
      public static void main(String[] args){
         String str;
         str = null;
-        System.out.println(str.length());
+        try{System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("例外発生だあああ！");
+            System.out.println(e.getMessage());
+
+        }
      }
 
 }
